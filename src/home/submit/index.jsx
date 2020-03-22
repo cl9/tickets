6bd0aa0d5a2cefc556cac9215b35c6516aed98c9
成林 <chenglin001@chinasofti.com>
2020-03-22 23:27:41 +0800

    修改Header,Journey,Submit文件夹的大小写问题

---
+++ src/home/submit/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './index.css'
+
+function Submit(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Submit.propTypes = {
+
+}
+
+export default Submit
+
